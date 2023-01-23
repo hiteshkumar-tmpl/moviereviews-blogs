fb9ea7d6d9dfb570f5aec4c0333c8fb3c624f4bb
hiteshkumar-tmpl <hitesh.k@tunicalabsmedia.com>
2023-01-23 13:05:52 +0530

    hamburger menu

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,8 +2,15 @@ import { NavLink } from "react-router-dom";
 import "../Navbar/Navbar.css";
 import { BsSearch } from "react-icons/bs";
 import logo from "../Navbar/onmyscreen.png";
+import { useState } from "react";
+import { FiAlignJustify } from "react-icons/fi";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
+  const [showIcon, setShowIcon] = useState(false);
+  const handleClick = () => {
+    setShowIcon(!showIcon);
+  };
   return (
     <div>
       <div className="navbar">
@@ -28,13 +35,14 @@ const Navbar = () => {
 
             <div className="dropdown"></div>
           </div>
-          <div className="navlink">
+          <div className={showIcon ? "navlink nav-active" : "navlink"}>
             <ol>
               <li>
                 {" "}
                 <NavLink
                   exact
                   to="/"
+                  onClick={handleClick}
                   className={({ isActive }) =>
                     isActive ? "active" : "inactive"
                   }
@@ -48,6 +56,7 @@ const Navbar = () => {
                 <NavLink
                   exact
                   to="/about"
+                  onClick={handleClick}
                   className={({ isActive }) =>
                     isActive ? "active" : "inactive"
                   }
@@ -60,6 +69,7 @@ const Navbar = () => {
                 <NavLink
                   exact
                   to="/contact"
+                  onClick={handleClick}
                   className={({ isActive }) =>
                     isActive ? "active" : "inactive"
                   }
@@ -68,6 +78,23 @@ const Navbar = () => {
                 </NavLink>
               </li>
             </ol>
+          </div>
+          <div className="hamburger">
+            <button
+              style={{
+                border: "none",
+                background: "none",
+                outline: "none",
+                cursor: "pointer",
+              }}
+              onClick={() => setShowIcon(!showIcon)}
+            >
+              {showIcon ? (
+                <RxCross2 className="cross" />
+              ) : (
+                <FiAlignJustify className="bar" />
+              )}
+            </button>
           </div>
         </nav>
       </div>
