908e7fc2c2c566a0f700e6e35780ba91d2ffe4e7
hiteshkumar-tmpl <hitesh.k@tunicalabsmedia.com>
2023-01-24 12:24:02 +0530

    mobile responisve test 2

--- src/Components/SingleBlogPage/SingleBlog.js
+++ src/Components/SingleBlogPage/SingleBlog.js
@@ -88,9 +88,12 @@ const SingleBlog = () => {
           </div>
 
           <div className="shortdescs">
-            I'm a paragraph. Click here to add your own text and edit me. It’s
-            easy. Just click “Edit Text” or double click me to add your own
-            content and make changes to the font.
+            <p>
+              {" "}
+              I'm a paragraph. Click here to add your own text and edit me. It’s
+              easy. Just click “Edit Text” or double click me to add your own
+              content and make changes to the font.
+            </p>
           </div>
           <div className="readmorebutton">
             <Link to="/about">
