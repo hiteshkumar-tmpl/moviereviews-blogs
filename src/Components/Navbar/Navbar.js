8bc3bcefcfe65d60309d09f4d523076c562aeedf
hiteshkumar-tmpl <hitesh.k@tunicalabsmedia.com>
2023-01-29 12:31:45 +0530

    form changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,16 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "../Navbar/Navbar.css";
 import { BsSearch } from "react-icons/bs";
 import logo from "../Navbar/onmyscreen.png";
 import { FiAlignJustify } from "react-icons/fi";
 import { RxCross2 } from "react-icons/rx";
+import axios from "axios";
 
 const Navbar = () => {
   const [showIcon, setShowIcon] = useState(false);
   const handleClick = () => {
     setShowIcon(!showIcon);
   };
+  const [search, setSearch] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const getSuggestions = async () => {
+      try {
+        const response = await axios.get(
+          ` https://onmyscreen.onrender.com/blogs/search?q=${search}`
+        );
+        const data = response.data;
+        setSuggestions(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (search) {
+      getSuggestions();
+    } else {
+      setSuggestions([]);
+    }
+  }, [search]);
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <div>
       <div className="navbar">
@@ -25,13 +53,19 @@ const Navbar = () => {
               <input
                 type="search"
                 name="search"
-                id="searchbar"
+                value={search}
+                onChange={handleChange}
                 placeholder="Search..."
               />
               <button className="button">
                 <BsSearch />
               </button>
             </div>
+            <ul>
+              {suggestions.map((suggestion) => (
+                <li key={suggestion}>{suggestion}</li>
+              ))}
+            </ul>
           </div>
           <div className={showIcon ? "navlink nav-active" : "navlink"}>
             <ol>
